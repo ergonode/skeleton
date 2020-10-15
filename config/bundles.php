43f60e54b59498d96a93b6292a6d8cb33059631c
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2020-10-15 14:52:07 +0200

    Update composer to ergonode 1.0.0-beta

--- config/bundles.php
+++ config/bundles.php
@@ -46,4 +46,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
