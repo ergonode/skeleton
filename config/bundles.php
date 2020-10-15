acaaad2909eb32111f9b2b9ceb8778a86a4aa9ba
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2020-10-15 15:37:56 +0200

    release 1.0.0-beta update readme files

--- config/bundles.php
+++ config/bundles.php
@@ -45,5 +45,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true]
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
